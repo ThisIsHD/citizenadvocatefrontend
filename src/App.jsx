e00aba37574a7c2504c46f8ca30f31520b320f02
Sebanti2003 <dasguptasebanti2003@gmail.com>
2025-02-05 18:47:53 +0530

    Initialized the whole repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-3xl capitalize'>hellooo citizens</div>
+    </>
+  )
+}
+
+export default App
